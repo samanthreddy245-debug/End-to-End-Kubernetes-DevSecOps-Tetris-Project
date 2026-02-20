eb380942b41b6c20d16267c6232bac7f089e6276
samanthreddy245-debug <samanthreddy245@gmail.com>
2026-02-20 12:49:34 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "samanth-terraform-state-bucket"
     region       = "us-east-1"
-    key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate
+    key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     encrypt      = true
   }
   required_version = ">=1.14.0"
